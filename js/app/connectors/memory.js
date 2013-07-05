86e8013e021b3e81186bd7712d4b101a3fb978b6
mluto <m@luto.at>
2013-07-05 19:11:24 +0200

    implemented memory.js and tests

--- js/app/connectors/memory.js
+++ js/app/connectors/memory.js
@@ -1,12 +1,114 @@
 (function ()
 {
   var self = {};
+  var notes = [];
+  var events =
+  {
+    "noteAdded": [],   // (index, note)
+    "noteRemoved": [], // (index, note)
+    "noteEdited": []   // (index, note, changed)
+  };
 
   self.init = function (options, cb)
   {
     console.log("memory init", options);
     cb();
   };
+
+  self.addNote = function (note, cb)
+  {
+    notes.push(note);
+
+    cb();
+    triggerEvent('noteAdded', [notes.length - 1, note]);
+  };
+
+  self.removeNote = function (index, cb)
+  {
+    if(index < 0 || index >= notes.length)
+      return cb("Invalid index");
+
+    var note = notes[index];
+    notes.splice(index, 1);
+
+    cb();
+    triggerEvent('noteRemoved', [index, note]);
+  };
+
+  self.editNote = function (index, newNote, cb)
+  {
+    if(index < 0 || index >= notes.length)
+      return cb("Invalid index");
+
+    var oldNote = notes[index];
+    var keys = getKeys(oldNote, newNote);
+
+    var changed = keys.filter(
+      function (key)
+      {
+        return oldNote[key] != newNote[key];
+      }
+    );
+
+    notes[index] = newNote;
+
+    cb();
+    triggerEvent('noteEdited', [index, newNote, changed]);
+  };
+
+  self.getNotes = function (cb)
+  {
+    cb(null, notes);
+  };
+
+  self.getNote = function (index, cb)
+  {
+    if(index < 0 || index >= notes.length)
+      return cb("Invalid index");
+
+    cb(null, notes[index]);
+  };
+
+  self.addEventReceiver = function (event, cb)
+  {
+    if(Object.keys(events).indexOf(event) == -1)
+      throw "Unknown Event";
+
+    events[event].push(cb);
+  };
+
+  function triggerEvent(event, args)
+  {
+    if(Object.keys(events).indexOf(event) == -1)
+      throw "Unknown Event";
+
+    for (var i = 0; i < events[event].length; i++)
+    {
+      var cb = events[event][i];
+      cb.apply(null, args);
+    }
+  }
+
+  function getKeys()
+  {
+    var keys = [];
+
+    for (var i = 0; i < arguments.length; i++)
+    {
+      var arg = arguments[i];
+      var argKeys = Object.keys(arg);
+
+      for (var j = 0; j < argKeys.length; j++)
+      {
+        var argKey = argKeys[j];
+
+        if(keys.indexOf(argKey) == -1)
+          keys.push(argKey);
+      }
+    }
+
+    return keys;
+  }
 
   shownoteseditor.connectors.memory = self;
 })();
---
+++ test/specs/connector.js
@@ -0,0 +1,301 @@
+var shownoteseditor = null;
+
+(function ()
+{
+  var connector;
+  var connectorName = "memory";
+
+  module("connector",
+    {
+      setup: function()
+      {
+        shownoteseditor = { connectors: {} };
+
+        jQuery.ajax({
+          url: "../js/app/connectors/" + connectorName + ".js",
+          dataType: "script",
+          async: false
+        });
+
+        connector = shownoteseditor.connectors[connectorName];
+        connector.init({}, function (){});
+      },
+      teardown: function()
+      {
+        shownoteseditor = null;
+        connector = null;
+      }
+    }
+  );
+
+  asyncTest("addNote callback",
+    function ()
+    {
+      connector.addNote({},
+        function ()
+        {
+          ok(true);
+          start();
+        }
+      );
+    }
+  );
+
+  asyncTest("addNote event",
+    function ()
+    {
+      var addNote = { a: "b" };
+
+      connector.addEventReceiver("noteAdded",
+        function (index, note)
+        {
+          equal(index, 0, "event - index ok");
+          deepEqual(note, addNote, "event - note ok");
+          start();
+        }
+      );
+
+      connector.addNote(addNote, function () {});
+    }
+  );
+
+  asyncTest("addNote",
+    function ()
+    {
+      var note = { a: "b" };
+
+      connector.addNote(note,
+        function ()
+        {
+          connector.getNotes(function (err, notes)
+            {
+              equal(notes.length, 1, "One note added");
+              deepEqual(notes[0], note, "Right note added");
+            }
+          );
+
+          start();
+        }
+      );
+    }
+  );
+
+  asyncTest("removeNote callback",
+    function ()
+    {
+      connector.addNote({ a: "b3" },
+        function (err)
+        {
+          connector.removeNote(0,
+            function ()
+            {
+              ok(true);
+              start();
+            }
+          );
+        }
+      );
+    }
+  );
+
+  asyncTest("removeNote event",
+    function ()
+    {
+      var addNote = { a: "b" };
+
+      connector.addEventReceiver("noteRemoved",
+        function (index, note)
+        {
+          equal(index, 0, "event - index ok");
+          deepEqual(note, addNote, "event - note ok");
+          start();
+        }
+      );
+
+      connector.addNote(addNote,
+        function (err)
+        {
+          connector.removeNote(0, function () {});
+        }
+      );
+    }
+  );
+
+  asyncTest("removeNote",
+    function ()
+    {
+      var addNotes =
+        [
+          { a: "b1" },
+          { a: "b2" },
+          { a: "b3" }
+        ];
+
+      async.series(
+        [
+          function (cb)
+          {
+            async.each(addNotes, connector.addNote, cb);
+          },
+          function (cb)
+          {
+            connector.removeNote(1, cb); // remove b2
+          },
+          function (cb)
+          {
+            connector.getNotes(function (err, notes)
+              {
+                equal(notes.length, 2, "Note removed");
+                equal(addNotes[0], notes[0], "Note 1 untouched");
+                equal(addNotes[2], notes[1], "Note 3 untouched");
+                cb();
+              }
+            );
+          }
+        ],
+        start
+      );
+    }
+  );
+
+  asyncTest("removeNote - invalid index",
+    function ()
+    {
+      connector.addEventReceiver("noteRemoved",
+        function (index, note)
+        {
+          ok(false, "Event fired");
+        }
+      );
+
+      connector.removeNote(1,
+        function (err)
+        {
+          equal(err, "Invalid index", "Error ok");
+          start();
+        }
+      );
+    }
+  );
+
+  asyncTest("editNote callback",
+    function ()
+    {
+      connector.addNote({},
+        function ()
+        {
+          connector.editNote(0, { a: "b" },
+            function ()
+            {
+              ok(true);
+              start();
+            }
+          );
+        }
+      );
+    }
+  );
+
+  asyncTest("editNote event",
+    function ()
+    {
+      var addNote = { a: "val1", b: "val2" };
+      var editedNote = { a: "val1", b: "newVal2" };
+
+      connector.addEventReceiver("noteEdited",
+        function (index, note, changed)
+        {
+          equal(index, 0, "event - index ok");
+          deepEqual(note, editedNote, "event - note ok");
+          deepEqual(changed, ["b"], "event - changed ok");
+          start();
+        }
+      );
+
+      connector.addNote(addNote,
+        function ()
+        {
+          connector.editNote(0, editedNote, function () {});
+        }
+      );
+    }
+  );
+
+  asyncTest("editNote",
+    function ()
+    {
+      var addNote = { a: "val1", b: "val2" };
+      var editedNote = { a: "val1", b: "newVal2" };
+
+      connector.addNote(addNote,
+        function (err)
+        {
+          connector.editNote(0, editedNote,
+            function (err)
+            {
+              connector.getNotes(function (err, notes)
+                {
+                  deepEqual(notes[0], editedNote, "Note changed");
+                  start();
+                }
+              );
+            }
+          );
+        }
+      );
+    }
+  );
+
+  asyncTest("editNote - invalid index",
+    function ()
+    {
+      connector.addEventReceiver("noteEdited",
+        function (index, note)
+        {
+          ok(false, "Event fired");
+        }
+      );
+
+      connector.editNote(1, {},
+        function (err)
+        {
+          equal(err, "Invalid index", "Error ok");
+          start();
+        }
+      );
+    }
+  );
+
+  asyncTest("getNote",
+    function ()
+    {
+      var addNote = { a: "val1", b: "val2" };
+
+      connector.addNote(addNote,
+        function (err)
+        {
+          connector.getNote(0,
+            function (err, note)
+            {
+              deepEqual(addNote, note, "Right note returned");
+              start();
+            }
+          );
+        }
+      );
+    }
+  );
+
+  asyncTest("getNote - invalid index",
+    function ()
+    {
+      connector.getNote(1,
+        function (err)
+        {
+          equal(err, "Invalid index", "Error ok");
+          start();
+        }
+      );
+    }
+  );
+})();
