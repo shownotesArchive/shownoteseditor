60dbe21c6e4d69679dcf4c6eda4a074269ea18ac
mluto <m@luto.at>
2013-08-03 19:35:31 +0200

    implemented audiojs player

--- js/app/players/audiojs.js
+++ js/app/players/audiojs.js
@@ -5,8 +5,42 @@
   shownoteseditor.players.pwp = function (options, cb)
   {
     console.log("audiojs init", options);
-    cb();
+
+    audiojs.events.ready(
+      function()
+      {
+        var audiojsOptions = {};
+
+        this.audio = audiojs.create(options.element, audiojsOptions);
+        cb();
+      }
+    );
   };
+
+  self.play = function ()
+  {
+    this.audio.play();
+  };
+
+  self.pause = function ()
+  {
+    this.audio.pause();
+  };
+
+  self.setCurrentTime = function (time)
+  {
+    this.audio.currentTime = time;
+  };
+
+  self.getCurrentTime = function ()
+  {
+    return this.audio.currentTime;
+  };
+
+  self.jumpTime = function (time)
+  {
+    this.setCurrentTime(getCurrentTime() + time);
+  }
 
   shownoteseditor.players.pwp.prototype = self;
 })();
