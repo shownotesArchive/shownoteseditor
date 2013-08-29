30257e97748616845552f367fae3126dca21b00e
mluto <m@luto.at>
2013-08-29 14:39:36 +0200

    use normalize.css (index.html, setup.sh) updated .gitignore

--- setup.sh
+++ setup.sh
@@ -4,3 +4,9 @@ cd js/lib
 wget "https://raw.github.com/caolan/async/master/lib/async.js" -O async.js
 wget "https://raw.github.com/jamuhl/i18next/master/release/i18next-1.6.3.min.js" -O i18next-1.6.3.min.js
 wget "https://raw.github.com/jeromeetienne/microevent.js/master/microevent.js" -O microevent.js
+cd ../..
+
+mkdir css/lib
+cd css/lib
+wget "http://necolas.github.io/normalize.css/2.1.3/normalize.css" -O normalize.css
+cd ../..
