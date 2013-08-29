7cd66cb961739285a1a61a6ea489a22d5406d26e
mluto <m@luto.at>
2013-08-29 13:50:14 +0200

    added microevent.js to setup.sh

--- setup.sh
+++ setup.sh
@@ -3,3 +3,4 @@ mkdir js/lib
 cd js/lib
 wget "https://raw.github.com/caolan/async/master/lib/async.js" -O async.js
 wget "https://raw.github.com/jamuhl/i18next/master/release/i18next-1.6.3.min.js" -O i18next-1.6.3.min.js
+wget "https://github.com/jeromeetienne/microevent.js/blob/master/microevent.js" -O microevent.js
