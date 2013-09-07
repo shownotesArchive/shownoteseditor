dc78a923fb6d0fb377e5eb7ae764fb24dfb7fc6f
mluto <m@luto.at>
2013-09-07 19:40:35 +0200

    don't destroy the element the standard list should be placed in

--- js/app/lists/standard.js
+++ js/app/lists/standard.js
@@ -24,7 +24,7 @@
   {
     console.log("List-Standard init", options);
 
-    $(options.element).replaceWith(mainTemplate);
+    $(options.element).append(mainTemplate);
     this.element = $("ul.list.standard:first");
 
     cb();
